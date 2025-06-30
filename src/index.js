9102945e319e97d1076d6634562410399f921e26
ishpreet404 <ishpreet@outlook.in>
2025-06-30 17:23:43 +0530

    iffy approach

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";  //always use async await
+import { DB_NAME } from "./constants";
+import express from "express"
+const app = express();
+
+
+
+
+
+
+
+
+// ; (async () => {
+//     try {
+//         await mongoose.connect('${process.env.MONGODB_URI}/${DB_NAME}');
+//         app.on("error", (error) => {
+//             console.log("error", error);
+//             throw error;
+//         })
+//         app.listen(process.env.PORT)
+//     } catch (error) {
+//         console.error("error", error);
+//         throw error
+        
+//     };
+    
+// })()
